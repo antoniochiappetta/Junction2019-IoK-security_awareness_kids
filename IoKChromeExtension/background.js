0bfc0c3b509386b9402f7ed5078b33845287467a
Antonio Chiappetta <antonychiappetta@gmail.com>
2019-11-16 18:27:18 +0200

    Auto stash before merge of "master" and "origin/master"

--- IoKChromeExtension/background.js
+++ IoKChromeExtension/background.js
@@ -1,5 +1,6 @@
 var activeTab = null;
 var use_case = null;
+// MARK: - Experience level
 
 chrome.tabs.onRemoved.addListener(function(tabid, removed) {
 
@@ -12,44 +13,64 @@ chrome.tabs.onRemoved.addListener(function(tabid, removed) {
         });
     }
 });
+function get_experience(category) {
+    const correctKey = category + "_correct";
+    const wrongKey = category + "_wrong";
+    const correctValue = localStorage.getItem(correctKey);
+    const wrongValue = localStorage.getItem(wrongKey);
+    const correct = correctValue == undefined ? 0 : correctValue;
+    const wrong = wrongValue == undefined ? 0 : wrongValue;
+    return correct / (correct + wrong + 1);
+}
 
+// MARK: - Phishing
 
-// how to redirect
 chrome.webRequest.onBeforeRequest.addListener(
     function(details) {
-
-        if (Math.random() > 0.5) {
+        exp_level = get_experience("phishing");
+        prob_limit = 0.7 // Ensure always no more than 30% probability of being redirected
+        if (Math.random() > Math.max(prob_limit, exp_level)) {
+            return {redirectUrl: chrome.extension.getURL("./components/phishing/phishing.html")};
+        }
             chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
                 activeTab = tabs[0].id;
                 use_case = "ransomware"
             });
-            return {redirectUrl: chrome.runtime.getURL("./components/ransomware/ransomware_redirect.html")};
-        }//put here the url you want to redirect the page to
     },
     {
-        urls: [
-            // put here the list of links that need to react to this event e.g. "*://www.google.com/*"
-            "*://www.piratebay.se/*",
-            "*://www.google.com/*"
-        ],
-        types: ["main_frame", "sub_frame", "stylesheet", "script", "image", "object", "xmlhttprequest", "other"]
-    },
-    ["blocking"] // makes request synchronous, does not load until function returns
-);
-
-// MARK: - Phishing action
-
-chrome.webRequest.onBeforeRequest.addListener(
-    function(details) {
-        return {redirectUrl: chrome.extension.getURL("./components/phishing/phishing.html")};
-    },
-   {
        urls: [
            "*://www.facebook.com/*"
        ],
-       types: ["main_frame", "sub_frame", "stylesheet", "script", "image", "object", "xmlhttprequest", "other"]
-   },
+       types: ["main_frame", "sub_frame"]
+    },
    ["blocking"]
+);
+
+// MARK: - Ransomware
+
+chrome.webRequest.onBeforeRequest.addListener(
+    function(details) {
+        exp_level = get_experience("ransomware");
+        prob_limit = 0.7 // Ensure always no more than 30% probability of being redirected
+        if (Math.random() > Math.max(prob_limit, exp_level)) {
+            return {redirectUrl: chrome.runtime.getURL("./components/ransomware/ransomware_redirect.html")};
+        }
+    },
+    {
+        urls: [
+            // Social networks
+            "*://www.facebook.com/*",
+            "*://www.twitter.com/*",
+            // Seach engines
+            "*://www.google.com/*",
+            "*://www.bing.com/*",
+            // Video streaming
+            "*://www.netflix.com/*",
+            "*://www.youtube.com/*"
+        ],
+        types: ["main_frame", "sub_frame"]
+    },
+    ["blocking"]
 );
 
 // how to open pop up pages
---
+++ IoKChromeExtension/utils/experience.js
@@ -0,0 +1,25 @@
+// MARK: - Experience
+
+function store_experience(category, answer) {
+    var key = ""
+    if (answer == "correct") {
+        key = category + "_correct";
+    } else {
+        key = category + "_wrong";
+    }
+    
+    const oldValue = localStorage.getItem(key) == undefined ? 0 : localStorage.getItem(key);
+    const value = oldValue + 1;
+    localStorage.setItem(key, value);
+    console.log(localStorage.getItem(key));
+  }
+  
+  function get_experience(category) {
+      const correctKey = category + "_correct";
+      const wrongKey = category + "_wrong";
+      const correctValue = localStorage.getItem(correctKey);
+      const wrongValue = localStorage.getItem(wrongKey);
+      const correct = correctValue == undefined ? 0 : correctValue;
+      const wrong = wrongValue == undefined ? 0 : wrongValue;
+      return correct / (correct + wrong);
+  }
