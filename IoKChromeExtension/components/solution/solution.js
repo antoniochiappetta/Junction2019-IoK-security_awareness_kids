0bb25a84817b73f3ac552795381200511ecc20e5
Luca Molteni <moltek96@gmail.com>
2019-11-17 01:59:42 +0200

    Add dynamic case description

--- IoKChromeExtension/components/solution/solution.js
+++ IoKChromeExtension/components/solution/solution.js
@@ -33,15 +33,31 @@ const answer = params["answer"];
 
 // Depending on the category use a different message
 
-var message = ""
+var message = "";
 switch (category) {
     case "phishing":
-        message = "Don't give away your data";
+        message = "Phishing is a way that criminals get sensitive information (like usernames or passwords). It is a method of social engineering. Very often, phishing is done by electronic mail. This mail appears to come from a bank or other service provider. It usually says that because of some change in the system, the users need to re-enter their usernames/passwords to confirm them. The emails usually have a link to a page that looks almost like that of the real bank.\n";
         break;
     case "ransomware":
-        message = "Don't believe to anything";
+        message = "Ransomware can infect your machine if you accidentally pick up malware from a website or download!\n" +
+            "\n" +
+            "It operates in an alarming way, locking your PC out of the blue and accusing you of committing a crime, before demanding payment to make your system usable again.\n"+
+            "\n"+
+            "Never pay the 'fine'. Most of the time is just a fake attack, your computer is fine and you just need to close the current tab and any other popups; \n" +
+            "If you can't access your PC you can still try to remove the malaware asking your parents to run an anti-virus.\n\r" +
+            "1. Don't click on attachments, banners or links without knowing their true origin.\n\r" +
+            "2. Don't install or run non-trusted or unknown software.\n\r" +
+            "3. Don't install mobile apps from unknown providers/sources.";
+        break;
+    case "fb_fake_request":
+        message = "NEVER accept friendship requests from people you do not know on social networks!\n" +
+            "\n" +
+            "There are make bad intentioned people who fakes their identity on social networks and pretend to be your friends\n" +
+            "Never engage a conversation or provide any kind of information to these individuals!\n" +
+            "Immediately tell your parents.";
         break;
 }
+
 
 // Depending on the answer use a different reactionVideo
 
@@ -53,6 +69,10 @@ window.onload = function() {
     document.getElementById("reactionVideo").load();
 
     document.getElementById("result_rect").style.background = rect_color;
+
+    console.log(document.getElementById("bubble-heading"));
+    document.getElementById("bubble-heading").innerText = category;
+    document.getElementById("bubble-description").innerHTML = message;
 };
 
 // Depending on the answer show a different reaction color
