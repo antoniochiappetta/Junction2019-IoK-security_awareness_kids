466f8564bff3218e0beed41aedd20d4bf96e965a
fabiolatorre <fabio_latorre@live.it>
2019-11-17 03:31:56 +0200

    fixed ajax in fb_fake_request

--- IoKChromeExtension/components/fb_fake_request/fb_fake_request.js
+++ IoKChromeExtension/components/fb_fake_request/fb_fake_request.js
@@ -29,11 +29,11 @@ function store_experience(category, answer) {
     console.log(localStorage.getItem(key));
 }
 
-
 exp_level = get_experience("fb_fake_request");
 console.log("fb_fake_request exp:", exp_level);
 prob_limit = 0.7; // Ensure always no more than 30% probability of being redirected
-    if (Math.random() > Math.max(prob_limit, exp_level)) {
+if (Math.random() > Math.max(prob_limit, exp_level)) {
+// if (1) { // set the probability to 1, just for debug
     $(document).ready(function () {
         var el = document.getElementsByClassName('_4962')[0];
         var newEl = document.createElement('button');
@@ -42,45 +42,48 @@ prob_limit = 0.7; // Ensure always no more than 30% probability of being redirec
 
         el.parentNode.replaceChild(newEl, el);
 
-        // popup.innerHTML = '<div id="fake_modal" class="fake_modal"><!-- Modal Content --><div class="fake_modal-content"><!-- Modal Header --><div class="fake_modal-header"><h3 class="">Friend requests</h3><div class="close fa fa-close"></div></div><!-- Modal Body --><div class="fake_modal-body"><img class="fake_pic" style="-webkit-user-select: none;margin: auto; border-radius: 100%;" width="100" src="chrome-extension://' + chrome.i18n.getMessage("@@extension_id") + '/components/fb_fake_request/pics/image.jpg"><div class="fake_name_container"><p class="fake_name">Alexis Brown</p><p>0 mutual friends</p><a href="#"><div class="fake_accept_button">Confirm</div></a><a href="#"><div class="fake_decline_button">Delete</div></a></div></div></div></div>';
-        var ajax = new XMLHttpRequest();
-        ajax.open("GET", "chrome-extension://" + chrome.i18n.getMessage("@@extension_id") + "/components/fb_fake_request/fb_fake_request.html", false);
-        ajax.send();
+        var xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function () {
+            if (this.readyState == 4 && this.status == 200) {
 
-        var popup = document.createElement('div');
-        popup.innerHTML = ajax.responseText;
-        document.getElementsByTagName("body")[0].appendChild(popup);
-
-        document.getElementsByClassName("fake_pic")[0].src = "chrome-extension://" + chrome.i18n.getMessage("@@extension_id") + "/components/fb_fake_request/pics/image.jpg";
-        var fake_request_inputs = document.getElementsByClassName("fake_request_input");
-        fake_request_inputs[0].action = "chrome-extension://" + chrome.i18n.getMessage("@@extension_id") + "/components/solution/solution.html"
-        fake_request_inputs[1].action = "chrome-extension://" + chrome.i18n.getMessage("@@extension_id") + "/components/solution/solution.html"
-
-        // Vars
-        var modBtn = $('#modBtn'),
-            fake_modal = $('#fake_modal'),
-            close = fake_modal.find('.close');
-
-        // open fake_modal when click on open fake_modal button 
-        modBtn.on('click', function () {
-            fake_modal.css('display', 'block');
-        });
-
-        // close fake_modal when click on close button or somewhere out the fake_modal content 
-        $(document).on('click', function (e) {
-            var target = $(e.target);
-            if (target.is(fake_modal) || target.is(close)) {
-                fake_modal.css('display', 'none');
+                var popup = document.createElement('div');
+                popup.innerHTML = this.responseText;
+                document.getElementsByTagName("body")[0].appendChild(popup);
+        
+                document.getElementsByClassName("fake_pic")[0].src = "chrome-extension://" + chrome.i18n.getMessage("@@extension_id") + "/components/fb_fake_request/pics/image.jpg";
+                var fake_request_inputs = document.getElementsByClassName("fake_request_input");
+                fake_request_inputs[0].action = "chrome-extension://" + chrome.i18n.getMessage("@@extension_id") + "/components/solution/solution.html"
+                fake_request_inputs[1].action = "chrome-extension://" + chrome.i18n.getMessage("@@extension_id") + "/components/solution/solution.html"
+        
+                // Vars
+                var modBtn = $('#modBtn'),
+                    fake_modal = $('#fake_modal'),
+                    close = fake_modal.find('.close');
+        
+                // open fake_modal when click on open fake_modal button 
+                modBtn.on('click', function () {
+                    fake_modal.css('display', 'block');
+                });
+        
+                // close fake_modal when click on close button or somewhere out the fake_modal content 
+                $(document).on('click', function (e) {
+                    var target = $(e.target);
+                    if (target.is(fake_modal) || target.is(close)) {
+                        fake_modal.css('display', 'none');
+                    }
+                });
+        
+                $("#fake_decline_button").on('click', function (e) {
+                    store_experience('fb_fake_request', 'correct')
+                });
+        
+                $("#fake_accept_button").on('click', function (e) {
+                    store_experience('fb_fake_request', 'wrong')
+                });
             }
-        });
-
-        $("#fake_decline_button").on('click', function (e) {
-            store_experience('fb_fake_request', 'correct')
-        });
-
-        $("#fake_accept_button").on('click', function (e) {
-            store_experience('fb_fake_request', 'wrong')
-        });
+        };
+        xhttp.open("GET", "chrome-extension://" + chrome.i18n.getMessage("@@extension_id") + "/components/fb_fake_request/fb_fake_request.html", true);
+        xhttp.send();
     });
 }
 
