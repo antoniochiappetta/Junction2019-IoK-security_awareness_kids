4140a2d86cf20b8f602f6f033874c53d6eced31e
Luca Molteni <moltek96@gmail.com>
2019-11-17 02:28:20 +0200

    Descriptions update

--- IoKChromeExtension/components/solution/solution.js
+++ IoKChromeExtension/components/solution/solution.js
@@ -36,28 +36,42 @@ const answer = params["answer"];
 var message = "";
 switch (category) {
     case "phishing":
-        message = "Phishing is a way that criminals get sensitive information (like usernames or passwords). It is a method of social engineering. Very often, phishing is done by electronic mail. This mail appears to come from a bank or other service provider. It usually says that because of some change in the system, the users need to re-enter their usernames/passwords to confirm them. The emails usually have a link to a page that looks almost like that of the real bank.\n";
+        message = "Phishing is a way that criminals get sensitive information (like usernames or passwords). It is a method of social engineering. Very often, phishing is done by electronic mail. This mail appears to come from a bank or other service provider. It usually says that because of some change in the system, the users need to re-enter their usernames/passwords to confirm them. The emails usually have a link to a page that looks almost like that of the real bank.<br>"+
+            "<br><br> Password are very very sensible information. It's important to never intentionally reveal them, but also having a good and secure password is fundamental to avoid malicious attempts to guess it."+
+            "<br>Click on the button below to learn more about secure passwords!;
         break;
     case "ransomware":
-        message = "Ransomware can infect your machine if you accidentally pick up malware from a website or download!\n" +
-            "\n" +
-            "It operates in an alarming way, locking your PC out of the blue and accusing you of committing a crime, before demanding payment to make your system usable again.\n"+
-            "\n"+
-            "Never pay the 'fine'. Most of the time is just a fake attack, your computer is fine and you just need to close the current tab and any other popups; \n" +
-            "If you can't access your PC you can still try to remove the malaware asking your parents to run an anti-virus.\n\r" +
-            "1. Don't click on attachments, banners or links without knowing their true origin.\n\r" +
-            "2. Don't install or run non-trusted or unknown software.\n\r" +
+        message = "Ransomware can infect your machine if you accidentally pick up malware from a website or download!<br>" +
+            "<br>" +
+            "It operates in an alarming way, locking your PC out of the blue and accusing you of committing a crime, before demanding payment to make your system usable again.<br>"+
+            "<br>"+
+            "Never pay the 'fine'. Most of the time is just a fake attack, your computer is fine and you just need to close the current tab and any other popups; <br>" +
+            "If you can't access your PC you can still try to remove the malaware asking your parents to run an anti-virus.<br><br>" +
+            "1. Don't click on attachments, banners or links without knowing their true origin.<br>" +
+            "2. Don't install or run non-trusted or unknown software.<br>" +
             "3. Don't install mobile apps from unknown providers/sources.";
         break;
     case "fb_fake_request":
-        message = "NEVER accept friendship requests from people you do not know on social networks!\n" +
-            "\n" +
-            "There are make bad intentioned people who fakes their identity on social networks and pretend to be your friends\n" +
-            "Never engage a conversation or provide any kind of information to these individuals!\n" +
+        message = "NEVER accept friendship requests from people you do not know on social networks!<br>" +
+            "<br>" +
+            "There are many bad intentioned people who fake their identities on social networks and pretend to be your friends<br>" +
+            "Never engage a conversation or provide any kind of information to these individuals!<br>" +
             "Immediately tell your parents.";
         break;
 }
 
+var title = "";
+switch (category) {
+    case "phishing":
+        title = "Phishing";
+        break;
+    case "ransomware":
+        title = "Ransomware";
+        break;
+    case "fb_fake_request":
+        title = "Suspicious friend request";
+        break;
+}
 
 // Depending on the answer use a different reactionVideo
 
@@ -71,7 +85,7 @@ window.onload = function() {
 
 
     console.log(document.getElementById("bubble-heading"));
-    document.getElementById("bubble-heading").innerText = category;
+    document.getElementById("bubble-heading").innerText = title;
     document.getElementById("bubble-description").innerHTML = message;
     this.document.getElementById("result_div").innerHTML = result_text;
     document.getElementById("result_rect").style.background = result_color;
